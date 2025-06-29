d25d74de0dd22beacc59b99fd108843fd70f2bb7
波萝 <XxBo_LuoxX@163.com>
2025-06-29 13:18:43 +0800

    1

---
+++ js/blog.js
@@ -0,0 +1,42 @@
+function loadBlogs() {
+    const blogList = document.getElementById('blog-list');
+    const blogs = ['blog/blog1.html', 'blog/blog2.html'];
+    const allTags = [];
+    blogs.forEach((blogPath) => {
+        const blogContainer = document.createElement('div');
+        fetch(blogPath)
+          .then(response => response.text())
+          .then(data => {
+                blogContainer.innerHTML = data;
+                const article = blogContainer.querySelector('article');
+                const tags = article.dataset.tags.split(',');
+                tags.forEach(tag => allTags.push(tag.trim()));
+                blogList.appendChild(blogContainer);
+            });
+    });
+    const uniqueTags = [...new Set(allTags)];
+    const categoryLink = document.getElementById('category-link');
+    uniqueTags.forEach(tag => {
+        const tagLink = document.createElement('a');
+        tagLink.href = '#';
+        tagLink.textContent = tag;
+        tagLink.classList.add('sub-category-link');
+        document.getElementById('sub-category-container').appendChild(tagLink);
+    });
+    const subCategoryLinks = document.querySelectorAll('.sub-category-link');
+    subCategoryLinks.forEach((link) => {
+        link.addEventListener('click', function () {
+            const targetTag = this.textContent;
+            const blogs = document.querySelectorAll('.blog-post');
+            blogs.forEach((blog) => {
+                const blogTags = blog.dataset.tags.split(',');
+                if (blogTags.includes(targetTag)) {
+                    blog.style.display = 'block';
+                } else {
+                    blog.style.display = 'none';
+                }
+            });
+        });
+    });
+}
+window.onload = loadBlogs;

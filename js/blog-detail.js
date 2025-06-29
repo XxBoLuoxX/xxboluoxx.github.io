53fa04d32961eb1ab7e72ae639110eefa8f134a5
波萝 <XxBo_LuoxX@163.com>
2025-06-29 16:27:47 +0800

    1

--- js/blog-detail.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-    const blogContent = localStorage.getItem('blogContent');
-    const detailContent = document.getElementById('blog-detail-content');
-    if (detailContent) {
-        detailContent.innerHTML = blogContent;
-    }
-};
